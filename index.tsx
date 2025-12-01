c54e319734d9fb8c931da3283d6f683a05f61ced
Lungisani <mjasti37@gmail.com>
2025-12-01 18:10:51 +0200

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,15 +1,2 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// React application disabled in favor of Vanilla JS Venture-OS dashboard.
+console.log("Venture-OS: React entry point disabled.");
